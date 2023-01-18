7207e0ef8fb161130be97ff72a3497da20f3a02d
Amaya Reed <amreed1@s.sfusd.edu>
2023-01-18 10:50:16 -0800

    Made vehicle able to turn with controls/arrow keys

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+
         // We'll move the Vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform. Translate(Vector3. right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
